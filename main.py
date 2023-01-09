5c58ae6cafd6c8930d84aed096fa555089935c27
Sachin Limbu <80691974+sachinlim@users.noreply.github.com>
2023-01-09 12:44:21 +0000

    adding convert_date() function to convert from month name to month number

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import pandas as pd
-pd.set_option('display.max_columns', 7)
 
 
 def extract_data():
     """
     Extracts the data from the .csv files to include only the columns needed
     :return: dataframe with the columns including: date, wakeup time, time slept, efficiency, quality,
-    deep sleep and SpO2 agerage.
+    deep sleep and SpO2 average.
     """
-
     df = pd.read_csv('AutoSleep Data/2022 - 2023.csv',
                      usecols=['toDate', 'waketime', 'asleep', 'efficiency', 'quality', 'deep', 'SpO2Avg'])
 
@@ -27,12 +25,44 @@ def remove_empty_spo2(data):
     return data
 
 
+def convert_date(data):
+    """
+    Converts dates to include the month with its numeric value before formatting the strings to datetime
+    :param data: dataframe with month names
+    :return: dataframe with month numbers
+    """
+    dates = {
+        'Jan': '01',
+        'Feb': '02',
+        'Mar': '03',
+        'Apr': '04',
+        'May': '05',
+        'Jun': '06',
+        'Jul': '07',
+        'Aug': '08',
+        'Sep': '09',
+        'Oct': '10',
+        'Nov': '11',
+        'Dec': '12',
+    }
+
+    # slicing the string so that it only includes the date.
+    data['Date'] = data['Date'].replace(dates, regex=True).str[-11:]
+
+    # removing the first value in the string because it is an integer, and formatting it as a date
+    data['Date'] = data['Date'].str.replace(',', '').str.replace(' ', '', 1).str.replace(' ', '-')
+
+    # converting the string to a datetime format
+    data['Date'] = pd.to_datetime(data['Date'], dayfirst=True)
+
+    return data
+
+
 def transform_data(data):
     """
     Transforming the data so that it is useful and all rows have data
     :return: data that has been cleaned
     """
-
     # renaming columns
     data.columns = ['Date', 'Wakeup Time', 'Hours Slept', 'Efficiency', 'Quality Sleep Time', 'Deep Sleep Time',
                     'Oxygen Saturation Average']
@@ -41,7 +71,8 @@ def transform_data(data):
     # cannot use datetime (%H:%M:%S) format as it is stored as a string
     data['Wakeup Time'] = data['Wakeup Time'].str.slice(11, 19)
 
-    data = remove_empty_spo2(data)
+    remove_empty_spo2(data)
+    convert_date(data)
 
     return data
 
@@ -49,4 +80,5 @@ def transform_data(data):
 extracted_data = extract_data()
 transformed_data = transform_data(extracted_data)
 
+pd.set_option('display.max_columns', 7)
 print(transformed_data)
